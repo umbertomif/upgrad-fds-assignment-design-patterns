293ca4f81a9ead74f986c453d39e188ca03dfce6
Umberto M. I. Ferreira <umberto.mif@gmail.com>
2023-10-28 22:33:47 +0900

    Implemented the chain of responsibility design pattern.

--- src/main/java/com/upgrad/patterns/Middleware/AuthenticationProcessor.java
+++ src/main/java/com/upgrad/patterns/Middleware/AuthenticationProcessor.java
@@ -6,10 +6,13 @@ public abstract class AuthenticationProcessor {
 
     protected AuthenticationProcessor nextProcessor;
 
-    public AuthenticationProcessor(AuthenticationProcessor nextProcessor)
-    {
+    public AuthenticationProcessor(AuthenticationProcessor nextProcessor) {
         this.nextProcessor = nextProcessor;
     }
 
     public abstract boolean isAuthorized(AuthenticationProvider provider);
+
+    public void setNextProcessor(AuthenticationProcessor nextProcessor) {
+        this.nextProcessor = nextProcessor;
+    }
 }
