ee1d6e63ecd730e5ce03bf56fe33a13406f00dd8
Umberto M. I. Ferreira <umberto.mif@gmail.com>
2023-10-28 22:52:49 +0900

    Added the facade design pattern.

--- src/main/java/com/upgrad/patterns/Service/DiseaseCountFacade.java
+++ src/main/java/com/upgrad/patterns/Service/DiseaseCountFacade.java
@@ -4,10 +4,14 @@ import com.upgrad.patterns.Constants.SourceType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static com.upgrad.patterns.Constants.SourceType.DiseaseSh;
+import static com.upgrad.patterns.Constants.SourceType.JohnHopkins;
+
 @Service
 public class DiseaseCountFacade {
 
    //create a private object indiaDiseaseStat of type IndiaDiseaseStatFactory
+   private IndiaDiseaseStatFactory indiaDiseaseStat;
 
     @Autowired
     public DiseaseCountFacade(IndiaDiseaseStatFactory indiaDiseaseStat)
@@ -15,19 +19,21 @@ public class DiseaseCountFacade {
         this.indiaDiseaseStat = indiaDiseaseStat;
     }
 
-    
     //create a public method getDiseaseShCount() that has Object as its return type
-    	//call the GetInstance method with DiseaseSh as the parameter using the indiaDiseaseStat object created on line 10
-    	//Based on the strategy returned, call the specific implementation of the GetActiveCount method
-    	//return the response
-   
-    
-    //create a public method getJohnHopkinCount() that has Object as its return type
-		//call the GetInstance method with JohnHopkins as the parameter using the indiaDiseaseStat object created on line 10
-		//Based on the strategy returned, call the specific implementation of the GetActiveCount method
-    	//return the response
-    
+    public Object getDiseaseShCount () {
+        //call the GetInstance method with DiseaseSh as the parameter using the indiaDiseaseStat object created on line 14
+        //Based on the strategy returned, call the specific implementation of the GetActiveCount method
+        //return the response
+        return indiaDiseaseStat.GetInstance(DiseaseSh).GetActiveCount();
+    }
 
+    //create a public method getJohnHopkinCount() that has Object as its return type
+    public Object getJohnHopkinCount () {
+        //call the GetInstance method with JohnHopkins as the parameter using the indiaDiseaseStat object created on line 14
+        //Based on the strategy returned, call the specific implementation of the GetActiveCount method
+        //return the response
+        return indiaDiseaseStat.GetInstance(JohnHopkins).GetActiveCount();
+    }
 
     public Object getInfectedRatio(String sourceType) throws IllegalArgumentException {
         try {
@@ -38,7 +44,7 @@ public class DiseaseCountFacade {
             return String.format("%.3f", percent * 100);
         }
         catch (Exception e) {
-            String message = String.format("Invalid source type specified. Available source type (%s, %s)", SourceType.DiseaseSh, SourceType.JohnHopkins);
+            String message = String.format("Invalid source type specified. Available source type (%s, %s)", DiseaseSh, JohnHopkins);
             throw new IllegalArgumentException(message);
         }
     }
